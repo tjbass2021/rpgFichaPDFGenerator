88b5070970603747b1292f7fc53bc19b9db7446b
tjbass2021 <tjbass2021@gmail.com>
2023-10-10 20:40:45 -0300

    Adição dos elementos na função de geração do PDF

--- main.py
+++ main.py
@@ -11,21 +11,31 @@ class TelaRPG:
         layout = [
             [Sg.Text('Informações Básicas', size=(23, 0), justification='center', pad=(15, 15), font='Arial 15 bold')],
             [Sg.Text('Nome: ', size=(6, 0)), Sg.Input(size=(30, 0), key='nome', background_color='white')],
+            [Sg.Text('Nome do Jogador: ', size=(10, 0)),
+             Sg.Input(size=(26, 0), key='nomejogador', background_color='white')],
             [Sg.Text('Raça: ', size=(6, 0)), Sg.Input(size=(30, 0), key='raca', background_color='white')],
             [Sg.Text('Classe: ', size=(6, 0)), Sg.Input(size=(30, 0), key='classe', background_color='white')],
             [Sg.Text('Nível: ', size=(6, 0)), Sg.Input(size=(30, 0), key='nivel', background_color='white')],
-            [Sg.Text('Antecedente: ', size=(11, 0)), Sg.Input(size=(25, 0), key='antecedente', background_color='white')],
-            [Sg.Text('Alinhamento: ', size=(11, 0)), Sg.Input(size=(25, 0), key='alinhamento', background_color='white')],
+            [Sg.Text('Antecedente: ', size=(11, 0)),
+             Sg.Input(size=(25, 0), key='antecedente', background_color='white')],
+            [Sg.Text('Alinhamento: ', size=(11, 0)),
+             Sg.Input(size=(25, 0), key='alinhamento', background_color='white')],
             [Sg.Text('Atributos', size=(22, 0), justification='center', pad=(15, 15), font='Arial 15 bold')],
 
             # Atributos
 
-            [Sg.Text('Força: ', size=(11, 0), justification='right'), Sg.Input(size=(5, 0), key='forca', background_color='white')],
-            [Sg.Text('Destreza: ', size=(11, 0), justification='right'), Sg.Input(size=(5, 0), key='destreza', background_color='white')],
-            [Sg.Text('Constituição: ', size=(11, 0), justification='right'), Sg.Input(size=(5, 0), key='constituicao', background_color='white')],
-            [Sg.Text('Inteligência: ', size=(11, 0), justification='right'), Sg.Input(size=(5, 0), key='inteligencia', background_color='white')],
-            [Sg.Text('Sabedoria: ', size=(11, 0), justification='right'), Sg.Input(size=(5, 0), key='sabedoria', background_color='white')],
-            [Sg.Text('Carisma: ', size=(11, 0), justification='right'), Sg.Input(size=(5, 0), key='carisma', background_color='white')],
+            [Sg.Text('Força: ', size=(11, 0), justification='right'),
+             Sg.Input(size=(5, 0), key='forca', background_color='white')],
+            [Sg.Text('Destreza: ', size=(11, 0), justification='right'),
+             Sg.Input(size=(5, 0), key='destreza', background_color='white')],
+            [Sg.Text('Constituição: ', size=(11, 0), justification='right'),
+             Sg.Input(size=(5, 0), key='constituicao', background_color='white')],
+            [Sg.Text('Inteligência: ', size=(11, 0), justification='right'),
+             Sg.Input(size=(5, 0), key='inteligencia', background_color='white')],
+            [Sg.Text('Sabedoria: ', size=(11, 0), justification='right'),
+             Sg.Input(size=(5, 0), key='sabedoria', background_color='white')],
+            [Sg.Text('Carisma: ', size=(11, 0), justification='right'),
+             Sg.Input(size=(5, 0), key='carisma', background_color='white')],
             [Sg.Text('', size=(1, 1))],
 
             [Sg.Button('Generate')],
@@ -45,14 +55,40 @@ class TelaRPG:
             raca = self.values['raca']
             classe = self.values['classe']
             nivel = self.values['nivel']
+            nomejogador = self.values['nomejogador']
+            alinhamento = self.values['alinhamento']
+            antecedente = self.values['antecedente']
+            # Atributos
+
+            forca = self.values['forca']
+            destreza = self.values['destreza']
+            constituicao = self.values['constituicao']
+            inteligencia = self.values['inteligencia']
+            sabedoria = self.values['sabedoria']
+            carisma = self.values['carisma']
+
             print(f'''
 Nome: {nome}
+Nome do Jogador: {nomejogador}
 Raça: {raca}
 Classe: {classe}
-Idade: {nivel}
+Nível: {nivel}
+Alinhamento: {alinhamento}
+Antecedente: {antecedente}
+
+ATRIBUTOS
+
+Força: {forca}
+Destreza: {destreza}
+Constituição: {constituicao}
+Inteligência: {inteligencia}
+Sabedoria: {sabedoria}
+Carisma: {carisma}
 ''')
             print(self.button)
-            pdf.generate(nome, raca, classe, nivel)
+            pdf.generate(nome, nomejogador, raca, classe, nivel, antecedente,
+                         alinhamento, forca, destreza, constituicao, inteligencia,
+                         sabedoria, carisma)
 
 
 tela = TelaRPG()
--- pdf.py
+++ pdf.py
@@ -1,14 +1,31 @@
 from reportlab.pdfgen import canvas
+
+
 # from reportlab.lib.pagesizes import A5
 
 
-def generate(nome, raca, classe, idade):
-    cnv = canvas.Canvas(f"Ficha_Personagem-{nome}.pdf")
-    cnv.drawString(40, 800, f'Nome: {nome}')
-    cnv.drawString(40, 780, f'Raça: {raca}')
-    cnv.drawString(40, 760, f'Classe: {classe}')
-    cnv.drawString(40, 740, f'Idade: {idade}')
-    cnv.save()
+def generate(nomepersonagem, nomejogador, raca, classe, nivel, antecedente, alinhamento,
+             forca, destreza, constituicao, inteligencia, sabedoria, carisma):
+    c = canvas.Canvas(f'{nomepersonagem}.pdf')
+    c.drawString(100, 800, f'NOME DO PERSONAGEM: {nomepersonagem}')
+    c.drawString(300, 800, f'NÍVEL: {nivel}')
+    c.drawString(100, 780, f'NOME DO JOGADOR: {nomejogador}')
+    c.drawString(300, 780, f'CLASSE: {classe}')
+    c.drawString(100, 760, f'RAÇA: {raca}')
+    c.drawString(300, 760, f'ALINHAMENTO: {alinhamento}')
+    c.drawString(100, 740, f'ANTECEDENTE: {antecedente}')
 
+    # ATRIBUTOS
+
+    c.drawString(220, 700, 'ATRIBUTOS')
+    c.drawString(120, 670, f'Força: {forca}')
+    c.drawString(250, 670, f'Destreza: {destreza}')
+    c.drawString(120, 650, f'Constituição: {constituicao}')
+    c.drawString(250, 650, f'Inteligência: {inteligencia}')
+    c.drawString(120, 630, f'Sabedoria: {sabedoria}')
+    c.drawString(250, 630, f'Carisma: {carisma}')
+
+    c.showPage()
+    c.save()
 
 # generate('Thiago', 'Humano', 'Estudante', '20')
