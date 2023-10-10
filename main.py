ca7f82cc7490d7207f5ea42b4e35a83e8f07a8fe
tjbass2021 <tjbass2021@gmail.com>
2023-10-10 06:21:38 -0300

    Adição de mais elementos na GUI

--- main.py
+++ main.py
@@ -9,12 +9,27 @@ class TelaRPG:
         Sg.change_look_and_feel('DarkTeal4')
         # layout
         layout = [
+            [Sg.Text('Informações Básicas', size=(23, 0), justification='center', pad=(15, 15), font='Arial 15 bold')],
             [Sg.Text('Nome: ', size=(6, 0)), Sg.Input(size=(30, 0), key='nome', background_color='white')],
-            [Sg.Text('Raça: ', size=(6, 0)), Sg.Input(size=(10, 0), key='raca', background_color='white')],
-            [Sg.Text('Classe: ', size=(6, 0)), Sg.Input(size=(10, 0), key='classe', background_color='white')],
-            [Sg.Text('Idade: ', size=(6, 0)), Sg.Input(size=(10, 0), key='idade', background_color='white')],
+            [Sg.Text('Raça: ', size=(6, 0)), Sg.Input(size=(30, 0), key='raca', background_color='white')],
+            [Sg.Text('Classe: ', size=(6, 0)), Sg.Input(size=(30, 0), key='classe', background_color='white')],
+            [Sg.Text('Nível: ', size=(6, 0)), Sg.Input(size=(30, 0), key='nivel', background_color='white')],
+            [Sg.Text('Antecedente: ', size=(11, 0)), Sg.Input(size=(25, 0), key='antecedente', background_color='white')],
+            [Sg.Text('Alinhamento: ', size=(11, 0)), Sg.Input(size=(25, 0), key='alinhamento', background_color='white')],
+            [Sg.Text('Atributos', size=(22, 0), justification='center', pad=(15, 15), font='Arial 15 bold')],
+
+            # Atributos
+
+            [Sg.Text('Força: ', size=(11, 0), justification='right'), Sg.Input(size=(5, 0), key='forca', background_color='white')],
+            [Sg.Text('Destreza: ', size=(11, 0), justification='right'), Sg.Input(size=(5, 0), key='destreza', background_color='white')],
+            [Sg.Text('Constituição: ', size=(11, 0), justification='right'), Sg.Input(size=(5, 0), key='constituicao', background_color='white')],
+            [Sg.Text('Inteligência: ', size=(11, 0), justification='right'), Sg.Input(size=(5, 0), key='inteligencia', background_color='white')],
+            [Sg.Text('Sabedoria: ', size=(11, 0), justification='right'), Sg.Input(size=(5, 0), key='sabedoria', background_color='white')],
+            [Sg.Text('Carisma: ', size=(11, 0), justification='right'), Sg.Input(size=(5, 0), key='carisma', background_color='white')],
+            [Sg.Text('', size=(1, 1))],
+
             [Sg.Button('Generate')],
-            [Sg.Output(size=(40, 20))]
+            [Sg.Output(size=(36, 5))]
 
         ]
 
@@ -29,15 +44,15 @@ class TelaRPG:
             nome = self.values['nome']
             raca = self.values['raca']
             classe = self.values['classe']
-            idade = self.values['idade']
+            nivel = self.values['nivel']
             print(f'''
 Nome: {nome}
 Raça: {raca}
 Classe: {classe}
-Idade: {idade}
+Idade: {nivel}
 ''')
             print(self.button)
-            pdf.generate(nome, raca, classe, idade)
+            pdf.generate(nome, raca, classe, nivel)
 
 
 tela = TelaRPG()
