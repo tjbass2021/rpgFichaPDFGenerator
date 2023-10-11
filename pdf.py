098802b518fda6f37e579bb8180a51c17b2de709
tjbass2021 <tjbass2021@gmail.com>
2023-10-11 11:20:47 -0300

    Melhorias na composição do arquivo PDF gerado

--- pdf.py
+++ pdf.py
@@ -6,24 +6,42 @@ from reportlab.pdfgen import canvas
 
 def generate(nomepersonagem, nomejogador, raca, classe, nivel, antecedente, alinhamento,
              forca, destreza, constituicao, inteligencia, sabedoria, carisma):
+    nomepersonagem = str(nomepersonagem).upper()
+    nomejogador = str(nomejogador).upper()
+    raca = str(raca).upper()
+    classe = str(classe).upper()
+    antecedente = str(antecedente).upper()
+    alinhamento = str(alinhamento).upper()
+
     c = canvas.Canvas(f'{nomepersonagem}.pdf')
-    c.drawString(100, 800, f'NOME DO PERSONAGEM: {nomepersonagem}')
-    c.drawString(300, 800, f'NÍVEL: {nivel}')
-    c.drawString(100, 780, f'NOME DO JOGADOR: {nomejogador}')
-    c.drawString(300, 780, f'CLASSE: {classe}')
-    c.drawString(100, 760, f'RAÇA: {raca}')
-    c.drawString(300, 760, f'ALINHAMENTO: {alinhamento}')
-    c.drawString(100, 740, f'ANTECEDENTE: {antecedente}')
+    c.drawImage('./ded.png', 370, 720, 60, preserveAspectRatio=True)
+    c.setFont('Helvetica-Bold', 16)
+    c.drawString(50, 800, 'DADOS')
+    c.setFont('Helvetica', 12)
+    c.drawString(50, 760, f'Nome do Personagem: {nomepersonagem}  Jogador: {nomejogador}')
+    c.drawString(50, 740, f'Nível: {nivel}  Classe: {classe}')
+    c.drawString(50, 720, f'Raça: {raca}')
+    c.drawString(50, 700, f'Alinhamento: {alinhamento}  Antecedente: {antecedente}')
+
+    c.setFont('Helvetica-Bold', 14)
 
     # ATRIBUTOS
 
-    c.drawString(220, 700, 'ATRIBUTOS')
-    c.drawString(120, 670, f'Força: {forca}')
-    c.drawString(250, 670, f'Destreza: {destreza}')
-    c.drawString(120, 650, f'Constituição: {constituicao}')
-    c.drawString(250, 650, f'Inteligência: {inteligencia}')
-    c.drawString(120, 630, f'Sabedoria: {sabedoria}')
-    c.drawString(250, 630, f'Carisma: {carisma}')
+
+    c.drawString(50, 660, 'ATRIBUTOS')
+    c.setFont('Helvetica', 12)
+    c.drawString(50, 620, f'Força: {forca}')
+    c.drawString(180, 620, f'Destreza: {destreza}')
+    c.drawString(50, 600, f'Constituição: {constituicao}')
+    c.drawString(180, 600, f'Inteligência: {inteligencia}')
+    c.drawString(50, 580, f'Sabedoria: {sabedoria}')
+    c.drawString(180, 580, f'Carisma: {carisma}')
+
+    # my_text = "Meu nome é Thiago\ntenho 29 anos"
+    # textobject = c.beginText(50, 500)
+    # for line in my_text.splitlines(False):
+    #     textobject.textLine(line.rstrip())
+    # c.drawText(textobject)
 
     c.showPage()
     c.save()
