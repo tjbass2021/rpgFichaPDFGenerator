4884261dcbcfad02c02dea4dd31b7700f2c37d7e
tjbass2021 <tjbass2021@gmail.com>
2023-10-11 22:19:53 -0300

    Criação da função de modificadores e correção de design do PDF

---
+++ functions.py
@@ -0,0 +1,35 @@
+def modificador(atributo):
+    mod = 0
+    if atributo == 1:
+        mod = -5
+    elif atributo == 2 or atributo == 3:
+        mod = -4
+    elif atributo == 4 or atributo == 5:
+        mod = -3
+    elif atributo == 6 or atributo == 7:
+        mod = -2
+    elif atributo == 8 or atributo == 9:
+        mod = -1
+    elif atributo == 10 or atributo == 11:
+        mod = 0
+    elif atributo == 12 or atributo == 13:
+        mod = 1
+    elif atributo == 14 or atributo == 15:
+        mod = 2
+    elif atributo == 16 or atributo == 17:
+        mod = 3
+    elif atributo == 18 or atributo == 19:
+        mod = 4
+    elif atributo == 20 or atributo == 21:
+        mod = 5
+    elif atributo == 22 or atributo == 23:
+        mod = 6
+    elif atributo == 24 or atributo == 25:
+        mod = 7
+    elif atributo == 26 or atributo == 27:
+        mod = 8
+    elif atributo == 28 or atributo == 29:
+        mod = 9
+    elif atributo == 30:
+        mod = 10
+    return mod
